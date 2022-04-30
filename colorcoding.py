15b2a94fa3c4a80d767b4dfd2c749d41fd6ab46a
garci5ca <72396531+garci5ca@users.noreply.github.com>
2022-04-30 11:48:20 -0400

    Add files via upload

---
+++ colorcoding.py
@@ -0,0 +1,13 @@
+def main():
+
+    graph = dict()
+    vertexes = list()
+
+    def color(vertexes, k):
+        for vertex in vertexes:
+            vertex[1] = k
+
+
+
+if __name__ == "__main__":
+    main()
