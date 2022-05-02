61790cbe0c248d9286f9dccfec5560a1bdf1a77c
garci5ca <72396531+garci5ca@users.noreply.github.com>
2022-05-01 23:23:55 -0400

    Update colorcoding.py

--- colorcoding.py
+++ colorcoding.py
@@ -1,11 +1,32 @@
 def main():
 
+    n = input()
+    e = input()
     graph = dict()
-    vertexes = list()
 
-    def color(vertexes, k):
-        for vertex in vertexes:
-            vertex[1] = k
+    for _ in e:
+        myList = input().split()
+        graph[(myList[1], myList[2])] = myList[0]
+
+    colors = dict()
+
+    def color(graph, k):
+        x = 0
+        for vertex in graph:
+            colors[vertex] = x
+            x += 1
+            if x >= k:
+                x = 0
+
+    color(graph, n)
+
+    visited = set()
+
+    for x in graph:
+        if x not in visited:
+            set.add(x)
+
+        
 
 
 
